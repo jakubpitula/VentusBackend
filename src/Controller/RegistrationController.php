d66ab07f7ccad473c27d70458a6a6b6565b80f7b
jakubpitula <szachmistrz2@gmail.com>
2019-10-22 23:59:39 +0200

    password assert

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -74,7 +74,7 @@ class RegistrationController extends BaseController
         if(!isset($data['birthday'])) return new JsonResponse(['error' => 'Birthday not set'], 401);
         if(!isset($data['messenger'])) return new JsonResponse(['error' => 'Messenger not set'], 401);
 
-        if(strlen($data['password']) < 6) return new JsonResponse(['error' => 'Password too short'], 401); 
+        if(strlen($data['password']) < 8) return new JsonResponse(['error' => 'Password too short'], 401); 
         if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) return new JsonResponse(['error' => 'Invalid email'], 401);
 
         if($isEmailUnique!==null) return new JsonResponse(['error' => 'Email already exists'], 401);
